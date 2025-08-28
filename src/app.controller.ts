eb978cbcf65bdb41ac3efdd4ebcf858535708547
John Doe <johndoe@example.com>
2025-08-28 10:23:40 -0300

    feat: add URL shortening functionality with Mongoose integration
    - Updated package.json and package-lock.json to include necessary dependencies: @nestjs/config, @nestjs/mongoose, and mongoose.
    - Modified AppController to return the shortened URL along with the original URL.
    - Implemented URL schema for MongoDB using Mongoose.
    - Enhanced AppService to generate a short URL using nanoid.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,13 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  @Post('cadastrarUrl')
+  constructor(private readonly appService: AppService) {}
+  @Post('cadastrar-url')
   async cadastrarUrl(@Body('originalUrl') originalUrl: string) {
-    return { receivedUrl: originalUrl };
+    const shortUrl = await this.appService.gerarShortUrl(originalUrl);
+    return {
+      receivedUrl: originalUrl,
+      shortenUrl: `localhost:3000/${shortUrl}`,
+    };
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
-
+import { nanoid } from 'nanoid';
 @Injectable()
 export class AppService {
-
+  gerarShortUrl(originalUrl): string {
+    const shortUrl = nanoid(6);
+    return shortUrl;
+  }
 }
